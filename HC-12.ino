030e3153e4e240d409f629f49ae86a9150b1536b
Rick <nikitalternative95@gmail.com>
2019-10-12 14:31:27 +0500

    7

--- HC-12.ino
+++ HC-12.ino
@@ -2,11 +2,13 @@
 
 SoftwareSerial mySerial(10, 11); // RX, TX
 
+int ID = 1111;
+
 String command_line;
 
 void setup() {
   Serial.begin(9600);
-
+  pinMode(3, OUTPUT);
   mySerial.begin(9600);
 }
 
@@ -18,6 +20,11 @@ void loop() { // run over and over
  
   if(mySerial.available() > 1){
     String input = mySerial.readString();
+    int recieveID = input.parseInt();
+
+    if (recieveID == ID) {
+      digitalWrite(3,HIGH);
+      }
     Serial.println(input);    
   }
   delay(20);
