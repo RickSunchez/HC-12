ba08fd62803fd8c0f4a11f131e11f18676dc26e5
Rick <nikitalternative95@gmail.com>
2019-10-12 15:07:38 +0500

    10

--- HC-12.ino
+++ HC-12.ino
@@ -3,7 +3,8 @@
 SoftwareSerial mySerial(10, 11); // RX, TX
 
 int ID = 1111;
-bool onReTrans = false;
+int recieveID = 0;
+bool onRT = false;
 
 String command_line;
 
@@ -21,15 +22,20 @@ void loop() { // run over and over
  
   if(mySerial.available() > 1){
     String input = mySerial.readString();
-    int recieveID = input.toInt();
+    if (input.toInt() != recieveID) {
+      onRT = true;
+      recieveID = input.toInt();
+      }
 
-    if (recieveID == ID && !onReTrans) {
+    if (recieveID == ID) {
       digitalWrite(3,HIGH);
       }
     else {
-      mySerial.println(recieveID);
-      digitalWrite(3,LOW);
-      onReTrans = true;
+      if (onRT) {
+        mySerial.println(recieveID);
+        digitalWrite(3,LOW);
+        onRT = false;
+        }
       }
     Serial.println(input);    
   }
